1bc5d5db04ae34155e09ff6746cb157930a5a215
Karen Olson <karmarie@gmail.com>
2022-05-12 15:49:47 -0500

    Refactor tests for #run and #display_board

--- lib/game.rb
+++ lib/game.rb
@@ -10,4 +10,5 @@ class Game
   def display_board
     puts "1 | 2 | 3\n-----------\n4 | 5 | 6\n-----------\n7 | 8 | 9"
   end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,26 +3,27 @@
 require 'game'
 
 describe 'Tic Tac Toe Game' do
-  it 'makes a new object' do
-    game = Game.new
 
-    expect(game).to be_instance_of(Game)
+  before(:example) do
+    @game = Game.new
   end
 
   context '#display_board' do
     it 'displays an empty board before any moves have been made' do
-      game = Game.new
-
-      expect{ game.display_board }.to output("1 | 2 | 3\n-----------\n4 | 5 | 6\n-----------\n7 | 8 | 9\n").to_stdout
+      expect{ @game.display_board }.to output(empty_board + "\n").to_stdout
     end
   end
 
   context '#run' do
     it 'displays an empty board when the game starts' do
-      game = Game.new
+      welcome_message = "Welcome to Tic Tac Toe!\n"
 
-      expect{ game.run }.to output("Welcome to Tic Tac Toe!\n1 | 2 | 3\n-----------\n4 | 5 | 6\n-----------\n7 | 8 | 9\n").to_stdout
+      expect{ @game.run }.to output(welcome_message + empty_board + "\n").to_stdout
     end
+  end
+
+  def empty_board
+    "1 | 2 | 3\n-----------\n4 | 5 | 6\n-----------\n7 | 8 | 9"
   end
 
 end
