d136aa7c7e513ce44920faafe42f00299b564970
Karen Olson <karmarie@gmail.com>
2022-05-12 15:28:35 -0500

    Write a test to display an empty board

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Game
-  puts 'Welcome to Tic Tac Toe!'
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,4 +8,11 @@ describe 'Tic Tac Toe Game' do
 
     expect(game).to be_instance_of(Game)
   end
+
+  it 'displays an empty board before any moves have been made' do
+    game = Game.new
+
+    expect{ game.display_board }.to output("1 | 2 | 3\n-----------\n4 | 5 | 6\n-----------\n7 | 8 | 9\n").to_stdout
+  end
+
 end
