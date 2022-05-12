b404611390f0bad462470ddd7abf6434d46f5dd2
Karen Olson <karmarie@gmail.com>
2022-05-12 15:41:23 -0500

    Pass test to display empty board when game starts

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,11 @@
 
 class Game
 
+  def run
+    puts 'Welcome to Tic Tac Toe!'
+    display_board
+  end
+
   def display_board
     puts "1 | 2 | 3\n-----------\n4 | 5 | 6\n-----------\n7 | 8 | 9"
   end
