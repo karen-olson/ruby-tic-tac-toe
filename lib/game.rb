dc52650264c5d6a308f9c29624bca61717b8a556
Karen Olson <karmarie@gmail.com>
2022-05-12 16:19:15 -0500

    Run linter and fix issues

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
+# Main class for Tic Tac Toe game
 class Game
-
   def run
     puts 'Welcome to Tic Tac Toe!'
     display_board
@@ -10,5 +10,4 @@ class Game
   def display_board
     puts "1 | 2 | 3\n-----------\n4 | 5 | 6\n-----------\n7 | 8 | 9"
   end
-
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,14 +3,13 @@
 require 'game'
 
 describe 'Tic Tac Toe Game' do
-
   before(:example) do
     @game = Game.new
   end
 
   context '#display_board' do
     it 'displays an empty board before any moves have been made' do
-      expect{ @game.display_board }.to output(empty_board + "\n").to_stdout
+      expect { @game.display_board }.to output("#{empty_board}\n").to_stdout
     end
   end
 
@@ -18,12 +17,11 @@ describe 'Tic Tac Toe Game' do
     it 'displays an empty board when the game starts' do
       welcome_message = "Welcome to Tic Tac Toe!\n"
 
-      expect{ @game.run }.to output(welcome_message + empty_board + "\n").to_stdout
+      expect { @game.run }.to output("#{welcome_message + empty_board}\n").to_stdout
     end
   end
 
   def empty_board
     "1 | 2 | 3\n-----------\n4 | 5 | 6\n-----------\n7 | 8 | 9"
   end
-
 end
