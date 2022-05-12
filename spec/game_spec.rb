8a3fb8cf86d43cc7c7e7c5f6081f078868690b5d
Karen Olson <karmarie@gmail.com>
2022-05-12 15:40:44 -0500

    Write test for displaying empty board when game starts

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,10 +9,20 @@ describe 'Tic Tac Toe Game' do
     expect(game).to be_instance_of(Game)
   end
 
-  it 'displays an empty board before any moves have been made' do
-    game = Game.new
+  context '#display_board' do
+    it 'displays an empty board before any moves have been made' do
+      game = Game.new
 
-    expect{ game.display_board }.to output("1 | 2 | 3\n-----------\n4 | 5 | 6\n-----------\n7 | 8 | 9\n").to_stdout
+      expect{ game.display_board }.to output("1 | 2 | 3\n-----------\n4 | 5 | 6\n-----------\n7 | 8 | 9\n").to_stdout
+    end
+  end
+
+  context '#run' do
+    it 'displays an empty board when the game starts' do
+      game = Game.new
+
+      expect{ game.run }.to output("Welcome to Tic Tac Toe!\n1 | 2 | 3\n-----------\n4 | 5 | 6\n-----------\n7 | 8 | 9\n").to_stdout
+    end
   end
 
 end
