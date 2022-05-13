444d1e45d569be7e2148a000bfc3d138910c53d1
Karen Olson <karmarie@gmail.com>
2022-05-13 10:01:43 -0500

    Apply linter changes

--- lib/game.rb
+++ lib/game.rb
@@ -4,7 +4,8 @@
 class Game
   @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-  BOARD_DISPLAY = "#{@board[0]} | #{@board[1]} | #{@board[2]}\n-----------\n#{@board[3]} | #{@board[4]} | #{@board[5]}" +
+  BOARD_DISPLAY = "#{@board[0]} | #{@board[1]} | #{@board[2]}" \
+                  "\n-----------\n#{@board[3]} | #{@board[4]} | #{@board[5]}" +
                   "\n-----------\n#{@board[6]} | #{@board[7]} | #{@board[8]}".freeze
 
   def initialize
