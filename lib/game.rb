e983a207722b10cb92145ac8743e30a8f442d837
Karen Olson <karmarie@gmail.com>
2022-05-13 09:58:00 -0500

    Make board display a constant

--- lib/game.rb
+++ lib/game.rb
@@ -2,21 +2,20 @@
 
 # Main class for Tic Tac Toe game
 class Game
-  
+  @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  BOARD_DISPLAY = "#{@board[0]} | #{@board[1]} | #{@board[2]}\n-----------\n#{@board[3]} | #{@board[4]} | #{@board[5]}" +
+                  "\n-----------\n#{@board[6]} | #{@board[7]} | #{@board[8]}".freeze
+
   def initialize
-    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-    @board_display = "#{@board[0]} | #{@board[1]} | #{@board[2]}\n-----------\n#{@board[3]} | #{@board[4]} | #{@board[5]}\n-----------\n#{@board[6]} | #{@board[7]} | #{@board[8]}"
-
     @welcome_message = 'Welcome to Tic Tac Toe!'
   end
-  
+
   def run
     print_to_console
   end
 
   def print_to_console
-    puts @welcome_message + "\n" + @board_display
+    puts "#{@welcome_message}\n#{BOARD_DISPLAY}"
   end
-
 end
