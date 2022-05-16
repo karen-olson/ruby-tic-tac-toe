b0c70a86069978b992b33ca63341e4654981f408
Karen Olson <karmarie@gmail.com>
2022-05-16 15:36:15 -0500

    Create test for console.output

--- spec/console_spec.rb
+++ spec/console_spec.rb
@@ -1,14 +1,31 @@
 require 'console'
 
 describe 'Console' do
+    class TestStdout
+        attr_reader :messages
+    
+        def initialize
+            @messages = []
+        end
+    
+        def puts(message)
+            messages << message
+            nil
+        end
+    end
     
     context '#output' do
         it 'prints the given message to the console' do
-            console = Console.new
+            stdout = TestStdout.new
+            console = Console.new(stdout)
 
             message = 'Hello World'
+
+            console.output(message)
             
-            expect { console.output(message) }.to output("#{message}\n").to_stdout
+            # Not asserting that puts works, asserting that the integration of puts works
+            # (i.e. that puts is called in console)
+            expect(stdout.messages).to include(message)
         end
     end
 end
