81fb71c9bb5d425caea3ddeb1021e57615a9f040
Karen Olson <karmarie@gmail.com>
2022-05-16 17:15:05 -0500

    Write test for BoardManager#mark_space

---
+++ spec/board_manager_spec.rb
@@ -0,0 +1,17 @@
+require 'board_manager'
+
+describe 'BoardManager' do
+    context '#mark_space' do
+        it 'places an X in the selected space on the board' do
+            board_manager = BoardManager.new
+            token = 'X'
+            space = 1
+
+            board_manager.mark_space(token, space)
+
+            expected_board = ['X', 2, 3, 4, 5, 6, 7, 8, 9]
+
+            expect(board_manager.board).to eq(expected_board)
+        end
+    end
+end
