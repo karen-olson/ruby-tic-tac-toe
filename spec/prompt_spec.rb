fd2c5cd446e841d34547ed3e3e950f6360980c30
Karen Olson <karmarie@gmail.com>
2022-05-16 16:52:17 -0500

    Add tests for prompt class

---
+++ spec/prompt_spec.rb
@@ -0,0 +1,13 @@
+require 'prompt'
+
+describe 'Prompt' do
+    context '#choose_a_space' do
+        it 'it prints the desired prompt to the console' do
+            prompt = Prompt.new 
+
+            choose_a_space = 'Choose a space. Enter 1-9:'
+
+            expect(prompt.choose_a_space).to eq(choose_a_space)
+        end
+    end
+end
