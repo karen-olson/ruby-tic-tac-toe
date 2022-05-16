bc75990e28b4f8fe344cd0cdf57630777da19ff6
Karen Olson <karmarie@gmail.com>
2022-05-16 15:37:02 -0500

    Extract Display class from Game

---
+++ lib/display.rb
@@ -0,0 +1,12 @@
+class Display
+
+    def present
+        <<~BOARD
+         1 | 2 | 3
+        ---+---+---
+         4 | 5 | 6
+        ---+---+---
+         7 | 8 | 9
+        BOARD
+    end
+end
