8f0abb8651fbc5f0d0e2a1ca2b8511a0a682a48b
Karen Olson <karmarie@gmail.com>
2022-05-16 15:36:41 -0500

    Create test for display.present

---
+++ spec/display_spec.rb
@@ -0,0 +1,19 @@
+require 'display'
+
+describe 'Display' do
+    context '#present' do
+        it 'presents a formatted 3x3 board' do
+            display = Display.new
+
+            board = <<~BOARD
+            1 | 2 | 3
+           ---+---+---
+            4 | 5 | 6
+           ---+---+---
+            7 | 8 | 9
+            BOARD
+
+            expect(display.present).to eq(board)
+        end
+    end
+end
