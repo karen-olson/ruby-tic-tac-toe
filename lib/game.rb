0b61d0e11b0d46f475c8e844aa6dfaf2fe359abe
Karen Olson <karmarie@gmail.com>
2022-05-16 16:52:39 -0500

    Use prompt class in game

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,17 @@
 class Game
-  attr_reader :console, :welcome_message, :display
+  attr_reader :console, :welcome_message, :display, :prompt
 
-  def initialize(console, welcome_message, display)
+  def initialize(console, welcome_message, display, prompt)
     @console = console
     @welcome_message = welcome_message
     @display = display
+    @prompt = prompt
   end
 
   def run
     welcome_player
     display_board
+    prompt_player_to_choose_space
   end
 
   private
@@ -20,5 +22,9 @@ class Game
 
   def display_board
     console.output(display.present)
+  end
+
+  def prompt_player_to_choose_space
+    console.output(prompt.choose_a_space)
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -18,26 +18,27 @@ class TestDisplay
   end
 end
 
+class TestPrompt
+  def choose_a_space
+    'choose'
+  end
+end
+
 describe 'Tic Tac Toe Game' do
   context '#run' do
     it 'plays the game' do
-      console = TestConsole.new
-      welcome_message = 'Hello player!'
-      display = TestDisplay.new
+        console = TestConsole.new
+        welcome_message = 'Hello player!'
+        display = TestDisplay.new
+        prompt = TestPrompt.new
 
-      game = Game.new(console, welcome_message, display)
+        game = Game.new(console, welcome_message, display, prompt)
 
-      game.run
+        game.run
 
-      expected_messages = [welcome_message, 'board']
+        expected_output = [welcome_message, 'board', 'choose']
 
-      expect(game.console.messages).to eq(expected_messages)
-
-      # How to build test double to make assertions about how the game class will
-      # interact with testconsole object? Assert on that instead.
-      # Log string pattern in TDD by Example (could be array).
-
-      # Build and test real console that you're going to inject in main (bin/run)
+        expect(game.console.messages).to eq(expected_output)
     end
   end
 end
