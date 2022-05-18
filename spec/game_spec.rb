a22a840e69dd153df6c48f5e68db35212a7b711b
Karen Olson <karmarie@gmail.com>
2022-05-18 16:12:55 -0500

    Add test doubles for BoardManager & gets_input to Game spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,35 +10,52 @@ class TestConsole
   def output(message)
     @messages << message
   end
+
+  def gets_input
+    1
+  end
 end
 
 class TestDisplay
-  def present
-    'board'
+  def present(board_values)
+    board_values
   end
 end
 
 class TestPrompt
   def choose_a_space
-    'choose'
+    'choose a space'
   end
 end
 
-describe 'Tic Tac Toe Game' do
+class TestBoardManager
+  attr_accessor :board_values
+
+  def initialize
+    @board_values = 'empty board'
+  end
+
+  def mark_space(_token, _space)
+    @board_values = 'board with one X mark'
+  end
+end
+
+describe 'Game' do
   context '#run' do
     it 'plays the game' do
-        console = TestConsole.new
-        welcome_message = 'Hello player!'
-        display = TestDisplay.new
-        prompt = TestPrompt.new
+      console = TestConsole.new
+      welcome_message = 'Hello player!'
+      display = TestDisplay.new
+      prompt = TestPrompt.new
+      board_manager = TestBoardManager.new
 
-        game = Game.new(console, welcome_message, display, prompt)
+      game = Game.new(console, welcome_message, display, prompt, board_manager)
 
-        game.run
+      game.run
 
-        expected_output = [welcome_message, 'board', 'choose']
+      expected_output = [welcome_message, 'empty board', 'choose a space', 'board with one X mark']
 
-        expect(game.console.messages).to eq(expected_output)
+      expect(game.console.messages).to eq(expected_output)
     end
   end
 end
