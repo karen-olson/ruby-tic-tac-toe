1a897a9f670aef4954e08f08f26cc5fd1891c722
Karen Olson <karmarie@gmail.com>
2022-05-18 16:11:01 -0500

    Add BoardManager test double to #present spec

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -1,11 +1,19 @@
 require 'display'
 
+class TestBoardManagerForEmptyBoard
+  def board_values
+    (1..9).to_a
+  end
+end
+
 describe 'Display' do
   context '#present' do
     it 'presents a formatted 3x3 board' do
+      board_manager = TestBoardManagerForEmptyBoard.new
+      board_values = board_manager.board_values
       display = Display.new
 
-      board = <<~BOARD
+      expected_board_output = <<~BOARD
          1 | 2 | 3
         ---+---+---
          4 | 5 | 6
@@ -13,7 +21,7 @@ describe 'Display' do
          7 | 8 | 9
       BOARD
 
-      expect(display.present).to eq(board)
+      expect(display.present(board_values)).to eq(expected_board_output)
     end
   end
 end
