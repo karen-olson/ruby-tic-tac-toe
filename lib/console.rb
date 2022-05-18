35a1d3fdb1dd907592ca79947ac5a41f9000f7b1
Karen Olson <karmarie@gmail.com>
2022-05-18 16:08:15 -0500

    Write console#gets_input method

--- lib/console.rb
+++ lib/console.rb
@@ -1,11 +1,16 @@
 class Console
-  attr_reader :stdout
+  attr_reader :stdout, :stdin
 
-  def initialize(stdout)
+  def initialize(stdout, stdin)
     @stdout = stdout
+    @stdin = stdin
   end
 
   def output(message)
     stdout.puts message
+  end
+
+  def gets_input
+    stdin.gets
   end
 end
