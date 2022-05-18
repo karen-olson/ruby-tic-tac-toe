7b03a4e26c95dd48b51018a9d32f02a74d422689
Karen Olson <karmarie@gmail.com>
2022-05-18 16:14:26 -0500

    In Game, get player input and place X on board

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,7 @@
 class Game
-  attr_reader :console, :welcome_message, :display, :prompt
+  attr_reader :console, :welcome_message, :display, :prompt, :board_manager
+
+  # attr_accessor :latest_move
 
   def initialize(console, welcome_message, display, prompt, board_manager)
     @console = console
@@ -7,14 +9,16 @@ class Game
     @display = display
     @prompt = prompt
     @board_manager = board_manager
+    @latest_move = nil
   end
 
   def run
     welcome_player
     display_board
     prompt_player_to_choose_space
+    gets_move
     place_mark_on_board
-    # display_board
+    display_board
   end
 
   private
@@ -24,26 +28,19 @@ class Game
   end
 
   def display_board
-    # display will need to take in the current board state 
-    # (board_manager.board)
-    console.output(display.present)
+    board_values = board_manager.board_values
+    console.output(display.present(board_values))
   end
 
   def prompt_player_to_choose_space
     console.output(prompt.choose_a_space)
   end
-  
-  # def get_player_token
-  # 
-  # end
 
-  # def get_player_input
-  #   # 
-  # end
+  def gets_move
+    @latest_move = console.gets_input.to_i
+  end
 
-  def place_mark_on_board(token, space)
-    # token = get_player_token
-    # space = get_player_input
-    # board_manager.mark_space(token, space)
+  def place_mark_on_board
+    board_manager.mark_space('X', @latest_move)
   end
 end
