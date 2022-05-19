7de5681a667812f709716027594c237c86406fe6
Karen Olson <karmarie@gmail.com>
2022-05-19 17:11:24 -0500

    Spike out Prompt#call

--- lib/prompt.rb
+++ lib/prompt.rb
@@ -1,4 +1,27 @@
 class Prompt
+  attr_accessor :console
+
+  def initialize(console:, number_validator:)
+    @console = console
+    @number_validator = number_validator
+  end
+
+  def call(message)
+  # def call(message, validate)
+    # get input from user
+    input = console.gets_input
+
+    # pass input into validate
+    if number_validator.valid?(input)
+      # if valid, maybe cast and return to user
+      input.to_i
+    else
+      # otherwise display error message and retry
+      console.output(message)
+      # retry
+    end
+  end
+
   def choose_a_space
     'Choose a space. Enter 1-9:'
   end
