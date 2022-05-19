b20402e8cd611008789ffd43621ec3ceb34f8afc
Karen Olson <karmarie@gmail.com>
2022-05-19 16:43:09 -0500

    Remove input validation from Board

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Board
   attr_accessor :values
 
@@ -13,25 +11,6 @@ class Board
   end
 
   def mark_space(token, space)
-    if valid_number?(space)
-      values[space - 1] = token
-    else
-      # what to do if there's incorrect input?
-      # who should be responsible for 
-      #     setting an error state in game?
-      #     displaying an error message to the user?
-      #     prompting for more input?
-      # return
-    end
+    values[space - 1] = token
   end
-
-  private 
-
-  def valid_number?(space)
-    space.is_a?(Integer) && (1..9).include?(space)
-  end
-
-  # def open?(space)
-  #   values[space - 1].is_a?(Integer)
-  # end
 end
