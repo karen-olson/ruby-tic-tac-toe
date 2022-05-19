9bddfe8a0dd8dc307a82a92a48ff7ac6a046acf8
Karen Olson <karmarie@gmail.com>
2022-05-19 15:15:20 -0500

    Rename board_values, add get_space to test double

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -1,16 +1,21 @@
 require 'display'
 
-class TestBoardManagerForEmptyBoard
-  def board_values
+class TestBoardForEmptyBoard
+  def values
     (1..9).to_a
+  end
+
+  def get_space(row, column)
+    index = (row * 3) + column
+    values[index]
   end
 end
 
 describe 'Display' do
   context '#present' do
     it 'presents a formatted 3x3 board' do
-      board_manager = TestBoardManagerForEmptyBoard.new
-      board_values = board_manager.board_values
+      board = TestBoardForEmptyBoard.new
+      values = board.values
       display = Display.new
 
       expected_board_output = <<~BOARD
@@ -21,7 +26,7 @@ describe 'Display' do
          7 | 8 | 9
       BOARD
 
-      expect(display.present(board_values)).to eq(expected_board_output)
+      expect(display.present(board)).to eq(expected_board_output)
     end
   end
 end
