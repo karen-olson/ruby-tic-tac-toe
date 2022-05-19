591e84cd09cc33e8cb31079ace08c1efd0e5bf09
Karen Olson <karmarie@gmail.com>
2022-05-19 15:17:34 -0500

    Rename variables, remove type casting from Game

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,16 @@
+require 'pry'
+
 class Game
-  attr_reader :console, :welcome_message, :display, :prompt, :board_manager
+  attr_reader :console, :welcome_message, :display, :prompt, :board
 
   # attr_accessor :latest_move
 
-  def initialize(console, welcome_message, display, prompt, board_manager)
+  def initialize(console, welcome_message, display, prompt, board)
     @console = console
     @welcome_message = welcome_message
     @display = display
     @prompt = prompt
-    @board_manager = board_manager
+    @board = board
     @latest_move = nil
   end
 
@@ -28,8 +30,7 @@ class Game
   end
 
   def display_board
-    board_values = board_manager.board_values
-    console.output(display.present(board_values))
+    console.output(display.present(board))
   end
 
   def prompt_player_to_choose_space
@@ -37,10 +38,10 @@ class Game
   end
 
   def gets_move
-    @latest_move = console.gets_input.to_i
+    @latest_move = console.gets_input
   end
 
   def place_mark_on_board
-    board_manager.mark_space('X', @latest_move)
+    board.mark_space('X', @latest_move)
   end
 end
