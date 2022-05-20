233bbe37995459bcf88b834799389bfefdb58e4f
Karen Olson <karmarie@gmail.com>
2022-05-20 17:41:21 -0500

    Write Prompt#call logic

--- lib/prompt.rb
+++ lib/prompt.rb
@@ -1,28 +1,29 @@
+require 'pry'
+
 class Prompt
-  attr_accessor :console
+
+  attr_reader :console, :number_validator
 
   def initialize(console:, number_validator:)
     @console = console
     @number_validator = number_validator
   end
 
-  def call(message)
-  # def call(message, validate)
-    # get input from user
-    input = console.gets_input
+  def call(message, error_message)
+    valid_input = nil
 
-    # pass input into validate
-    if number_validator.valid?(input)
-      # if valid, maybe cast and return to user
-      input.to_i
-    else
-      # otherwise display error message and retry
+    until (valid_input)
       console.output(message)
-      # retry
-    end
-  end
+      input = console.gets_input
+      range = 1..9
 
-  def choose_a_space
-    'Choose a space. Enter 1-9:'
+      if number_validator.valid?(input, range)
+        valid_input = Integer(input)
+      else
+        console.output(error_message)
+      end
+    end
+
+    valid_input
   end
 end
