ec7c0db781b7bb21d8cb10da7f65e776bc282ed4
Karen Olson <karmarie@gmail.com>
2022-05-23 11:00:15 -0500

    Fix infinite loop for invalid input test condition

--- spec/prompt_spec.rb
+++ spec/prompt_spec.rb
@@ -16,63 +16,41 @@ class TestNumberValidatorForValidInput
   end
 end
 
-# class TestConsoleForInvalidInput
-#   attr_accessor :messages
+class TestConsoleForRepeatedPrompts
+  attr_reader :return_values
+  attr_accessor :messages
 
-#   def initialize
-#     @messages = []
-#   end
+  def initialize
+    @iteration = 0
+    @return_values = ["sadf", "3"]
+    @messages = []
+  end
 
-#   def output(message)
-#     messages << message
-#   end
+  def gets_input
+    return_value = return_values[@iteration]
+    @iteration += 1
+    return_value
+  end
 
-#   def gets_input
-#     "7asdf$@"
-#   end
-# end
+  def output(message)
+    messages << message
+  end
+end
 
-# class TestNumberValidatorForInvalidInput
-#   def valid?(input, range)
-#     false
-#   end
-# end
+class TestNumberValidatorForRepeatedPrompts
+  attr_reader :return_values
 
-# class TestConsoleForRepeatedPrompts
-#   attr_reader :return_values
-#   attr_accessor :messages
-
-#   def initialize
-#     @iteration = 0
-#     @return_values = ["sadf", "@#$(*", "3"]
-#     @messages = []
-#   end
-
-#   def gets_input
-#     return_value = return_values[@iteration]
-#     @iteration += 1
-#     return_value
-#   end
-
-#   def output(message)
-#     messages << message
-#   end
-# end
-
-# class TestNumberValidatorForRepeatedPrompts
-#   attr_reader :return_values
-
-#   def initialize
-#     @iteration = 0
-#     @return_values = [false, false, true]
-#   end
+  def initialize
+    @iteration = 0
+    @return_values = [false, true]
+  end
   
-#   def valid?(input, range)
-#     return_value = return_values[@iteration]
-#     @iteration += 1
-#     return_value
-#   end
-# end
+  def valid?(input, range)
+    return_value = return_values[@iteration]
+    @iteration += 1
+    return_value
+  end
+end
 
 describe 'Prompt' do
   context '#call' do
@@ -88,30 +66,16 @@ describe 'Prompt' do
       expect(valid_input).to eq(8)
     end
 
-    ### THESE TESTS MADE RSPEC MAD, BUT ONLY AFTER ADDING THE UNTIL LOOP
+    it 'continues prompting the user until valid input is received' do
+      console = TestConsoleForRepeatedPrompts.new
+      number_validator = TestNumberValidatorForRepeatedPrompts.new
+      prompt = Prompt.new(console: console, number_validator: number_validator)
 
-    # it 'displays an error message for invalid input' do
-    #   console = TestConsoleForInvalidInput.new
-    #   number_validator = TestNumberValidatorForInvalidInput.new
-    #   prompt = Prompt.new(console: console, number_validator: number_validator)
+      message = "Choose a space. Enter 1-9: "
+      error_message = "Please enter a valid number."
+      space_selection = prompt.call(message, error_message)
 
-    #   message = "Choose a space. Enter 1-9: "
-    #   error_message = "Please enter a valid number."
-    #   valid_input = prompt.call(message, error_message)
-
-    #   expect(console.messages).to include(error_message)
-    # end
-
-    # it 'continues prompting the user until valid input is received' do
-    #   console = TestConsoleForRepeatedPrompts.new
-    #   number_validator = TestNumberValidatorForRepeatedPrompts.new
-    #   prompt = Prompt.new(console: console, number_validator: number_validator)
-
-    #   message = "Choose a space. Enter 1-9: "
-    #   error_message = "Please enter a valid number."
-    #   space_selection = prompt.call(message, error_message)
-
-    #   expect(console.messages).to eq([message, error_message, message, error_message, message])
-    # end
+      expect(console.messages).to eq([message, error_message, message])
+    end
   end
 end
