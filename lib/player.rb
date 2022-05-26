9b4aaa8627b89738c49f9a0a9ebf572b8f7f10f6
Karen Olson <karmarie@gmail.com>
2022-05-26 15:48:10 -0500

    Add player class

---
+++ lib/player.rb
@@ -0,0 +1,11 @@
+class Player
+
+    # Should this be a value object? 
+    # Should it have a select method?
+
+    attr_reader :marker
+
+    def initialize(marker:)
+        @marker = marker
+    end
+end
