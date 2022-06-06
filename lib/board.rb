60611210ade74171d950e064f3c98a938cb280cf
Karen Olson <karmarie@gmail.com>
2022-06-06 09:55:12 -0500

    Add outcome checking to Board

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,17 @@
+require 'pry'
+
 class Board
   attr_accessor :values
+  attr_reader :outcome_checker, :dimension
 
-  def initialize
+  def initialize(outcome_checker:)
+    @outcome_checker = outcome_checker
     @values = (1..9).to_a
+    @dimension = Math.sqrt(values.length).to_i
   end
 
   def get_space(row, column)
-    index = (row * 3) + column
+    index = (row * dimension) + column
     values[index]
   end
 
@@ -15,7 +20,7 @@ class Board
   end
 
   def full?
-    open_board = (1..9).to_a
+    open_board = (1..values.length).to_a
     open_spaces = values & open_board
     open_spaces.empty?
   end
@@ -23,5 +28,75 @@ class Board
   def available?(input)
     space = values[input - 1]
     space.is_a?(Integer)
+  end
+
+  def has_win?
+    outcome_checker.win?(self)
+  end
+
+  def has_draw?
+    outcome_checker.draw?(self)
+  end
+
+  def rows
+    row_numbers.map { |row_number| row(row_number) }
+  end
+
+  def columns
+    column_numbers.map { |column_number| column(column_number) }
+  end
+
+  def diagonals
+    [left_diagonal, right_diagonal]
+  end
+
+  private
+
+  def row_numbers
+    max_row_number = dimension - 1
+    (0..max_row_number).to_a
+  end
+
+  def column_numbers
+    max_column_number = dimension - 1
+    (0..max_column_number).to_a
+  end
+
+  def row(row_number)
+    column_numbers.map do |column_number|
+      get_space(row_number, column_number)
+    end
+  end
+
+  def column(column_number)
+    row_numbers.map do |row_number|
+      get_space(row_number, column_number)
+    end
+  end
+
+  def left_diagonal
+    left_diagonal = []
+    current_row_or_column = 0
+
+    dimension.times do
+      left_diagonal << get_space(current_row_or_column, current_row_or_column)
+      current_row_or_column += 1
+    end
+
+    left_diagonal
+  end
+
+  def right_diagonal
+    right_diagonal = []
+    current_row = 0
+    current_column = dimension - 1
+
+    dimension.times do
+      right_diagonal << get_space(current_row, current_column)
+      current_row += 1
+      current_column -= 1
+    end
+
+    right_diagonal
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,24 @@
 require 'board'
 
+class TestOutcomeChecker
+end
+
 describe 'Board' do
+  context 'given a row and column number (from 0 - 2)' do
+    it '#get_space returns the correct value for the given coordinates' do
+      outcome_checker = TestOutcomeChecker.new
+      board = Board.new(outcome_checker:)
+      board.values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+      expect(board.get_space(0, 0)).to eq(1)
+      expect(board.get_space(2, 2)).to eq(9)
+    end
+  end
+
   context '#mark_space' do
     it 'places an X in the selected space on the board' do
-      board = Board.new
+      outcome_checker = TestOutcomeChecker.new
+      board = Board.new(outcome_checker:)
       token = 'X'
       space = 1
 
@@ -17,7 +32,8 @@ describe 'Board' do
 
   context '#full?' do
     it 'returns true when the board is full' do
-      board = Board.new
+      outcome_checker = TestOutcomeChecker.new
+      board = Board.new(outcome_checker:)
 
       board.values = %w[X O O X X O X X O]
 
@@ -25,7 +41,8 @@ describe 'Board' do
     end
 
     it 'returns false when the board is not full' do
-      board = Board.new
+      outcome_checker = TestOutcomeChecker.new
+      board = Board.new(outcome_checker:)
 
       board.values = ['X', 2, 'O', 'X', 'X', 'O', 'X', 'X', 'O']
 
@@ -35,17 +52,113 @@ describe 'Board' do
 
   context '#available?' do
     it 'returns true when the space is available' do
-      board = Board.new
+      outcome_checker = TestOutcomeChecker.new
+      board = Board.new(outcome_checker:)
+
       board.values = ['X', 2, 3, 'O', 5, 6, 'X', 8, 9]
 
       expect(board.available?(6)).to eq(true)
     end
 
     it 'returns false when the space is not available' do
-      board = Board.new
+      outcome_checker = TestOutcomeChecker.new
+      board = Board.new(outcome_checker:)
+
       board.values = ['X', 2, 3, 'O', 5, 6, 'X', 8, 9]
 
       expect(board.available?(7)).to eq(false)
+    end
+  end
+
+  context '#rows' do
+    it 'returns the correct row values' do
+      outcome_checker = TestOutcomeChecker.new
+      board = Board.new(outcome_checker:)
+      board.values = [
+        'O', 2, 'X',
+        4, 'X', 'O',
+        'O', 'X', 9
+      ]
+      expected_rows = [
+        ['O', 2, 'X'],
+        [4, 'X', 'O'],
+        ['O', 'X', 9]
+      ]
+
+      expect(board.rows).to eq(expected_rows)
+    end
+  end
+
+  context '#columns' do
+    it 'returns the correct column values' do
+      outcome_checker = TestOutcomeChecker.new
+      board = Board.new(outcome_checker:)
+      board.values = [
+        'O', 2, 'X',
+        4, 'X', 'O',
+        'O', 'X', 9
+      ]
+      expected_columns = [
+        ['O', 4, 'O'],
+        [2, 'X', 'X'],
+        ['X', 'O', 9]
+      ]
+
+      expect(board.columns).to eq(expected_columns)
+    end
+  end
+
+  context '#diagonals' do
+    it 'returns the correct diagonal values' do
+      outcome_checker = TestOutcomeChecker.new
+      board = Board.new(outcome_checker:)
+      board.values = [
+        'O', 2, 'X',
+        4, 'X', 'O',
+        'O', 'X', 9
+      ]
+      expected_diagonals = [
+        ['O', 'X', 9],
+        %w[X X O]
+      ]
+
+      expect(board.diagonals).to eq(expected_diagonals)
+    end
+  end
+
+  context '#has_draw?' do
+    it 'returns true when the outcome checker finds a draw' do
+      outcome_checker = TestOutcomeChecker.new
+      board = Board.new(outcome_checker:)
+      allow(outcome_checker).to receive(:draw?).and_return(true)
+
+      expect(board.has_draw?).to eq(true)
+    end
+
+    it 'returns false when the outcome checker does not find a draw' do
+      outcome_checker = TestOutcomeChecker.new
+      board = Board.new(outcome_checker:)
+      allow(outcome_checker).to receive(:draw?).and_return(false)
+
+      expect(board.has_draw?).to eq(false)
+    end
+  end
+
+  context '#has_win?' do
+    it 'returns true when the outcome checker finds a win' do
+      outcome_checker = TestOutcomeChecker.new
+      board = Board.new(outcome_checker:)
+      allow(outcome_checker).to receive(:win?).and_return(true)
+
+      expect(board.has_win?).to eq(true)
+    end
+
+    it 'returns false when the outcome checker does not find a win' do
+      outcome_checker = TestOutcomeChecker.new
+      board = Board.new(outcome_checker:)
+      allow(outcome_checker).to receive(:win?).and_return(false)
+
+      expect(board.has_win?).to eq(false)
     end
   end
 end
