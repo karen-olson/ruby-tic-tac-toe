42ce15bb8b6cc324825255513c1cb9bbcd3c49e2
Karen Olson <karmarie@gmail.com>
2022-06-09 17:01:23 -0500

    Test and create UI class

---
+++ lib/ui.rb
@@ -0,0 +1,18 @@
+class UI
+    def initialize(display:, prompter:)
+        @display = display 
+        @prompter = prompter
+    end
+
+    def display_board(board)
+        display.present(board)
+    end
+
+    def prompt
+        prompter.call
+    end
+
+    private 
+
+    attr_reader :display, :prompter
+end
