df8a09aae03fbf66e5f7d2bfc3a5ff41db04f7ba
Karen Olson <karmarie@gmail.com>
2022-06-13 15:35:24 -0500

    Delete Game, move integration tests to Game Looper

--- spec/game_looper_spec.rb
+++ spec/game_looper_spec.rb
@@ -1,4 +1,14 @@
 require 'game_looper'
+require 'console'
+require 'display'
+require 'prompt'
+require 'board'
+require 'player'
+require 'number_validator'
+require 'outcome_checker'
+require 'ui'
+require 'stringio'
+require 'pry'
 
 class TestConsole
   attr_reader :messages
@@ -71,8 +81,52 @@ end
 
 describe 'Game Looper' do
   describe '#loop' do
-    context 'until there is a draw' do
-      it 'loops' do
+    context 'given real objects', integration: true do
+      it 'loops until there is a draw' do
+        original_stdout = $stdout
+        $stdout = StringIO.new
+
+        console = Console.new(stdin: $stdin, stdout: $stdout)
+        board = Board.new
+        outcome_checker = OutcomeChecker.new(board:)
+        number_validator = NumberValidator.new
+        prompter = Prompt.new(console:, number_validator:, board:)
+        display = Display.new(console:)
+        ui = UI.new(display:, prompter:)
+        players = [Player.new(marker: 'X'), Player.new(marker: 'O')]
+        game_looper = GameLooper.new(ui:, board:, players:, outcome_checker:)
+
+        allow($stdin).to receive(:gets).and_return('7')
+        board.values = [
+          'X', '0', '0',
+          'O', 'O', 'X',
+          7, 'X', 'O'
+        ]
+
+        game_looper.loop
+
+        output = $stdout.string.split("\n")
+        expected_output = [
+          " X | 0 | 0", 
+          "---+---+---", 
+          " O | O | X", 
+          "---+---+---", 
+          " 7 | X | O", 
+          "Please choose a space.", 
+          " X | 0 | 0", 
+          "---+---+---", 
+          " O | O | X", 
+          "---+---+---", 
+          " X | X | O"
+        ]
+        expect(output).to eq(expected_output)
+
+        $stdout = original_stdout
+      end
+    end
+
+    context 'given test doubles' do
+      it 'loops until there is a draw' do
         ui = TestUI.new
         board = TestBoard.new
 
@@ -92,8 +146,52 @@ describe 'Game Looper' do
       end
     end
 
-    context 'until there is a win' do
-      it 'loops' do
+    context 'given real objects', integration: true do
+      it 'loops until there is a win' do
+        original_stdout = $stdout
+        $stdout = StringIO.new
+
+        console = Console.new(stdin: $stdin, stdout: $stdout)
+        board = Board.new
+        outcome_checker = OutcomeChecker.new(board:)
+        number_validator = NumberValidator.new
+        prompter = Prompt.new(console:, number_validator:, board:)
+        display = Display.new(console:)
+        ui = UI.new(display:, prompter:)
+        players = [Player.new(marker: 'X'), Player.new(marker: 'O')]
+        game_looper = GameLooper.new(ui:, board:, players:, outcome_checker:)
+
+        allow($stdin).to receive(:gets).and_return('9')
+        board.values = [
+          'X', 2,  3,
+           4, 'X', 6,
+           7,  8,  9
+        ]
+
+        game_looper.loop
+
+        output = $stdout.string.split("\n")
+        expected_output = [
+          " X | 2 | 3", 
+          "---+---+---", 
+          " 4 | X | 6", 
+          "---+---+---", 
+          " 7 | 8 | 9", 
+          "Please choose a space.", 
+          " X | 2 | 3", 
+          "---+---+---", 
+          " 4 | X | 6", 
+          "---+---+---", 
+          " 7 | 8 | X"
+        ]
+        expect(output).to eq(expected_output)
+
+        $stdout = original_stdout
+      end
+    end
+
+    context 'given test doubles' do
+      it 'loops until there is a win' do
         ui = TestUI.new
         board = TestBoard.new
 
