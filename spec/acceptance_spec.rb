d920467bacc2b493f01dbd91d795b24c4391be57
Karen Olson <karmarie@gmail.com>
2022-06-24 09:53:51 -0500

    Switch to open3#capture2 to remove unused  variable

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -2,7 +2,7 @@ require 'open3'
 
 describe 'Acceptance', integration: true do
   it 'plays a draw game' do
-    stdout_str, _stderr_str, status = Open3.capture3('ruby bin/run', stdin_data: "1\n2\n3\n5\n4\n6\n8\n7\n9\n")
+    stdout_str, status = Open3.capture2('ruby bin/run', stdin_data: "1\n2\n3\n5\n4\n6\n8\n7\n9\n")
 
     output = stdout_str.downcase
 
@@ -11,7 +11,7 @@ describe 'Acceptance', integration: true do
   end
 
   it 'plays a winning game' do
-    stdout_str, _stderr_str, status = Open3.capture3('ruby bin/run', stdin_data: "1\n4\n2\n5\n3\n")
+    stdout_str, status = Open3.capture2('ruby bin/run', stdin_data: "1\n4\n2\n5\n3\n")
 
     output = stdout_str.downcase
 
