ff942c152d884deede52f5a114c371e21efb2945
Karen Olson <karmarie@gmail.com>
2022-06-28 09:08:38 -0500

    Change test setup to reflect human v computer game

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -1,21 +1,23 @@
 require 'open3'
 
 describe 'Acceptance', integration: true do
-  it 'plays a draw game' do
-    stdout_str, status = Open3.capture2('ruby bin/run', stdin_data: "1\n2\n3\n5\n4\n6\n8\n7\n9\n")
+  context 'given a human player and a computer player' do
+    it 'plays a draw game' do
+      stdout_str, status = Open3.capture2('ruby bin/run', stdin_data: "2\n4\n6\n7\n9\n")
 
-    output = stdout_str.downcase
+      output = stdout_str.downcase
 
-    expect(output).to include('welcome', 'draw', 'goodbye')
-    expect(status.exitstatus).to eq(0)
-  end
+      expect(output).to include('welcome', 'draw', 'goodbye')
+      expect(status.exitstatus).to eq(0)
+    end
 
-  it 'plays a winning game' do
-    stdout_str, status = Open3.capture2('ruby bin/run', stdin_data: "1\n4\n2\n5\n3\n")
+    it 'plays a winning game' do
+      stdout_str, status = Open3.capture2('ruby bin/run', stdin_data: "1\n5\n9\n")
 
-    output = stdout_str.downcase
+      output = stdout_str.downcase
 
-    expect(output).to include('welcome', 'x is the winner', 'goodbye')
-    expect(status.exitstatus).to eq(0)
+      expect(output).to include('welcome', 'x is the winner', 'goodbye')
+      expect(status.exitstatus).to eq(0)
+    end
   end
 end
