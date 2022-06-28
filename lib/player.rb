297c012b8adbde14ddc286c98ae4714a2f5bf31c
Karen Olson <karmarie@gmail.com>
2022-06-28 09:02:57 -0500

    Make Player an abstract superclass

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,11 @@
 class Player
   attr_reader :marker
 
-  def initialize(marker:)
-    @marker = marker
+  def initialize(**opts)
+    @marker = opts[:marker]
+
+    post_initialize(opts)
   end
+
+  def post_initialize(opts); end
 end
